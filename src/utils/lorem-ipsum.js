f9c5c1f5fd36057d034ae90c7b21f989ff4a94fd
Vlad Denisov <vlad.a.denisov@gmail.com>
2023-05-10 21:09:13 +0300

    mock data

---
+++ src/utils/lorem-ipsum.js
@@ -0,0 +1,31 @@
+import {generateRandomInt} from './random';
+
+const words = [
+  'Got',
+  'ability',
+  'shop',
+  'recall',
+  'fruit',
+  'easy',
+  'dirty',
+  'giant',
+  'shaking',
+  'ground',
+  'weather',
+  'lesson',
+  'almost',
+  'square',
+  'forward',
+  'bend',
+  'cold',
+  'broken',
+  'distant',
+  'adjective.'
+];
+const getRandomWord = (firstLetterToUppercase = false) => {
+  const word = words[generateRandomInt(0, words.length - 1)];
+  return firstLetterToUppercase ? word.charAt(0).toUpperCase() + word.slice(1) : word;
+};
+export const generateWords = (length = 10) => {
+  return `${[...Array(length)].map((_, i) => getRandomWord(i === 0)).join(' ').trim() }.`;
+};
--- src/view/trip-event-view.js
+++ src/view/trip-event-view.js
@@ -1,40 +1,74 @@
 import BaseView from './base-view.js';
+import {
+  convertToDateTime,
+  convertToEventDate,
+  convertToEventDateTime,
+  convertToTime
+} from '../utils/converters';
+import {getRandomOffers} from '../mock/offer.js';
+import {randomDesinations} from '../mock/destination';
 
-const createTripEventTemplate = () =>
-  `<li class="trip-events__item">
+
+const createOffersTemplate = (offers) => {
+  return offers.map((offer) => `
+    <li class="event__offer">
+      <span class="event__offer-title">${offer.title}</span>
+      &plus;&euro;&nbsp;
+      <span class="event__offer-price">${offer.price}</span>
+    </li>
+  `).join('');
+};
+
+const createTripEventTemplate = (eventPoint) => {
+  const {destination, offers, type} = eventPoint;
+  const offersArray = getRandomOffers()
+    .find((e) => (e.type === type))['offers']
+    .filter((e) => (e.id in offers));
+  const eventDateTime = convertToEventDateTime(eventPoint.date_from);
+  const eventDate = convertToEventDate(eventPoint.date_from);
+  const fromDateTime = convertToDateTime(eventPoint.date_from);
+  const fromTime = convertToTime(eventPoint.date_from);
+  const toDateTime = convertToDateTime(eventPoint.date_to);
+  const toTime = convertToTime(eventPoint.date_to);
+  const offersTemplate = createOffersTemplate(offersArray);
+
+  return `<li class="trip-events__item">
     <div class="event">
-        <time class="event__date" datetime="2019-03-18">MAR 18</time>
+        <time class="event__date" datetime="${eventDateTime}">${eventDate}</time>
         <div class="event__type">
-            <img class="event__type-icon" width="42" height="42" src="img/icons/drive.png" alt="Event type icon">
+            <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
         </div>
-        <h3 class="event__title">Drive Chamonix</h3>
+        <h3 class="event__title">${randomDesinations.getDestination(destination).name}</h3>
         <div class="event__schedule">
             <p class="event__time">
-                <time class="event__start-time" datetime="2019-03-18T14:30">14:30</time>
+                <time class="event__start-time" datetime="${fromDateTime}">${fromTime}</time>
                     &mdash;
-                <time class="event__end-time" datetime="2019-03-18T16:05">16:05</time>
+                <time class="event__end-time" datetime="${toDateTime}">${toTime}</time>
             </p>
         </div>
         <p class="event__price">
-        &euro;&nbsp;<span class="event__price-value">160</span>
+        &euro;&nbsp;<span class="event__price-value">${eventPoint.base_price}</span>
         </p>
         <h4 class="visually-hidden">Offers:</h4>
         <ul class="event__selected-offers">
-            <li class="event__offer">
-                <span class="event__offer-title">Rent a car</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">200</span>
-            </li>
+            ${offersTemplate}
         </ul>
         <button class="event__rollup-btn" type="button">
             <span class="visually-hidden">Open event</span>
         </button>
     </div>
   </li>`;
+};
+
 
 class TripEventView extends BaseView {
+  constructor({tripPoint}) {
+    super();
+    this.tripPoint = tripPoint;
+  }
+
   getTemplate() {
-    return createTripEventTemplate();
+    return createTripEventTemplate(this.tripPoint);
   }
 }
 
